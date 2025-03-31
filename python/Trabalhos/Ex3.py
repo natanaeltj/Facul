7b3bb123f516ca129aef94e68a2e2da0fed15cf3
Natanael <natanaelteixeiradejesus2@gmail.com>
2025-03-31 08:15:18 -0300

    ex04.py

--- python/Trabalhos/Ex3.py
+++ python/Trabalhos/Ex3.py
@@ -1,7 +1,6 @@
 print('Bem vindo a copiadora de Natanael Teixeira \n')
 
-
-def escolha_servico():
+def escolha_servico(): #Realiza a seleção do serviço
     servico = ''
     while(servico not in ('DIG', 'ICO', 'IPB', 'FOT')):
         print('Entre com o tipo de serviço desejado: \n DIG - Digitalização \n ICO - Impressão colorida \n IPB - Impressão Preto e Branco \n FOT - Fotocópia')
@@ -11,7 +10,7 @@ def escolha_servico():
         elif(servico in ('DIG', 'ICO', 'IPB', 'FOT')):
             return servico
 
-servico_E = escolha_servico()
+servico_E = escolha_servico() #Salva custo por pagina do serviço selecionado
 if servico_E == 'DIG':
     custo_p_pag = 1.10
 elif servico_E == 'ICO':
@@ -21,7 +20,7 @@ elif servico_E == 'IPB':
 elif servico_E == 'FOT':
     custo_p_pag = 0.20
 
-def num_pagina():
+def num_pagina(): #define o número de paginas escolhido
     x = 0
     while(True):
 
@@ -35,7 +34,8 @@ def num_pagina():
             print('Esse não é um número válido!! Por favor tente novamente')
 
 paginas = num_pagina()
-if paginas >= 20 and paginas < 200:
+
+if paginas >= 20 and paginas < 200: #Define o valor do desconto pelo número de páginas
     desconto = 15 / 100
 elif paginas >= 200 and paginas < 2000:
     desconto =  20 / 100
@@ -43,11 +43,10 @@ elif paginas >= 2000 and paginas < 20000:
     desconto = 25 / 100
 else:
     desconto = 0
-paginas_desconto = paginas - (paginas * desconto)
+paginas_desconto = paginas - (paginas * desconto) #calcula o desconto
 
 
-
-def servico_extra():
+def servico_extra(): #retorna o valor do serviço requisitado
     servico_add = ''
     while(servico_add not in ('1', '2','0')):
         print('Deseja adicioar algum serviço? \n 1 - Encardenação Simples - R$ 15.00 \n 2 - Encardenação Capa Dura - R$ 40.00 \n 0 - Não desejo mais nada')
@@ -62,13 +61,9 @@ def servico_extra():
             print('Valor invalido, por favor insira novamente')
 extra = servico_extra()
 
+total = (custo_p_pag * paginas_desconto) + extra #calcula o valor total
 
-
-
-
-total = (custo_p_pag * paginas_desconto) + extra
-
-if extra == None:
+if extra == None: #mostra o valor total e a função sem serviço e com serviço adicional
     print(f'Total: R$ {total} (serviço: R${custo_p_pag}) * páginas: {paginas} (SEM SERVIÇO ADICIONAL)')
 else:
     print(f'Total: R$ {total} serviço: R$ {custo_p_pag} * páginas: {paginas} + extra: R$ {extra}')
